21a563a935056a4631a592a4de49bf1681c62eb3
evgeny <zaborostroj@gmail.com>
2026-06-16 10:56:01 +0400

    basic sql queries

--- sql-tests/src/test/java/org/example/sql/BasicSqlQueryTest.java
+++ sql-tests/src/test/java/org/example/sql/BasicSqlQueryTest.java
@@ -20,7 +20,7 @@ import java.sql.Statement;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
-public class SqlQueryTest {
+public class BasicSqlQueryTest {
 
     @Container
     static PostgreSQLContainer<?> database = new PostgreSQLContainer<>("postgres:16")
@@ -48,11 +48,16 @@ public class SqlQueryTest {
         liquibase.update(new Contexts(), new LabelExpression());
     }
 
+    /**
+     * Количество сотрудников
+     */
     @Test
     void testCountEmployees() throws Exception {
         String sql = "SELECT COUNT(*) FROM employees";
-        try (Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+        try (
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery(sql)
+        ) {
             rs.next();
             int count = rs.getInt(1);
 
@@ -60,15 +65,61 @@ public class SqlQueryTest {
         }
     }
 
+    /**
+     * Вторая максимальная зарплата
+     */
     @Test
     void testSecondMaxSalary() throws Exception {
         String sql = "SELECT DISTINCT salary FROM employees ORDER BY salary DESC LIMIT 1 OFFSET 1";
-        try (Statement stmt = connection.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) {
+        try (
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery(sql)
+        ) {
             rs.next();
             double salary = rs.getDouble(1);
 
             assertEquals(120000.00, salary, 0.001);
+        }
+    }
+
+    /**
+     * Сотрудники без отдела
+     */
+    @Test
+    void testDepartmentIsNull() throws Exception {
+        String sql = "SELECT COUNT(*) FROM employees WHERE department_id IS NULL";
+        try (
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery(sql)
+        ) {
+            rs.next();
+            int count = rs.getInt(1);
+
+            assertEquals(1, count);
+        }
+    }
+
+    /**
+     * Сотрудники с зарплатой больше 100_000
+     */
+    @Test
+    void testSalaryBiggerThan() throws Exception {
+        String sql = "SELECT id, salary FROM employees WHERE salary > 130000 ORDER BY salary DESC, id ASC";
+        try (
+                Statement statement = connection.createStatement();
+                ResultSet rs = statement.executeQuery(sql)
+        ) {
+            rs.next();
+            assertEquals(2, rs.getLong(1));
+            assertEquals(135000L, rs.getLong(2));
+
+            rs.next();
+            assertEquals(4, rs.getLong(1));
+            assertEquals(135000L, rs.getLong(2));
+
+            rs.next();
+            assertEquals(16, rs.getLong(1));
+            assertEquals(135000L, rs.getLong(2));
         }
     }
 }
